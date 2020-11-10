6865134b4dc2c04fc075fb2412ae9ec33b6a9f86
Alexey <manesio2009@gmail.com>
2020-11-10 15:22:04 +0200

    Add Header and Button

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from '../Button/Button';
+import logoSvg from '../../assets/img/pizza-logo.svg';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <div className="container">
+          <div className="header__logo"> 
+            <img width="38" src={logoSvg} alt="Pizza" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
+          </div>
+          <div className="header__cart">
+           <Button />
+          </div>
+        </div>
+      </header>
+    )
+}
+
+export default Header;
