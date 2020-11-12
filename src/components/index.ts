00ab72cece09710ceb807c8ecba18f82eccc676c
Alexey <manesio2009@gmail.com>
2020-11-12 12:24:34 +0200

    add db and pizzaBlock

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Categories } from './Categories/Categories';
 export { default as Header } from './Header/Header';
 export { default as Sort } from './Sort/Sort';
+export { default as PizzaBlock } from './PizzaBlock/PizzaBlock';
