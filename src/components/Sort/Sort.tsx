bdb2b3d42679575dbcc95e6b4c22435f4901f657
Alexey <manesio2009@gmail.com>
2020-11-13 17:53:21 +0200

    Some

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
 
+
+
+
 function Sort(props:any) {
     const [visible, setVisible] = useState(false);
     const [activeItem, setActiveItem] = useState(0);
@@ -26,8 +29,7 @@ function Sort(props:any) {
                   height="6"
                   viewBox="0 0 10 6"
                   fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                  xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
                     fill="#2C2C2C"
@@ -39,7 +41,6 @@ function Sort(props:any) {
               
            {visible && (<div className="sort__popup">
                 <ul>
-
                 {props.items && props.items.map(
                     (obj:any, index:number) => (
                         <li className = {activeItem === index ? 'active' : ''}
@@ -49,10 +50,8 @@ function Sort(props:any) {
                             }} key={`${obj.type}_${index}`}>{obj.name}</li>
                     )
                 )}
-
                 </ul>
             </div>)}
-
             </div>
     )
 }
